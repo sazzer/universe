678d8d4b2e8a7b6e06b2b4486a25865793a0f3ab
Graham Cox <graham@grahamcox.co.uk>
2020-11-16 07:56:37 +0000

    Config and logging

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,3 +1,15 @@
-console.log('Hello');
+import 'dotenv/config';
 
-export const hello = 'world';
+import * as env from 'env-var';
+
+import debug from 'debug';
+
+const LOG = debug('universe');
+
+const config = {
+  http: {
+    port: env.get('PORT').default('8000').asPortNumber()
+  }
+};
+
+LOG('Configuration: %o', config);
