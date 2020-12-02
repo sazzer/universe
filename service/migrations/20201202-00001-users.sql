d8497430a62268c3b23fb2f2e6b15c1b468cd759
Graham Cox <graham@grahamcox.co.uk>
2020-12-02 17:19:22 +0000

    Migration for the users database table

---
+++ service/migrations/20201202-00001-users.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users(
+  user_id UUID PRIMARY KEY,
+  version UUID NOT NULL,
+  created TIMESTAMP WITH TIME ZONE NOT NULL,
+  updated TIMESTAMP WITH TIME ZONE NOT NULL,
+  username TEXT NULL UNIQUE,
+  email TEXT NULL UNIQUE,
+  display_name TEXT NOT NULL,
+  authentications JSONB NOT NULL
+);
