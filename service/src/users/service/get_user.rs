3e12d2607af999b439876c9509b4e3e457ac5d7c
Graham Cox <graham@grahamcox.co.uk>
2020-12-04 07:35:15 +0000

    Service layer test for getting users

--- service/src/users/service/get_user.rs
+++ service/src/users/service/get_user.rs
@@ -8,3 +8,28 @@ impl GetUserUseCase for UsersService {
         self.repository.get_user(user_id).await
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use crate::database::Database;
+
+    use super::*;
+    use assert2::check;
+    use universe_testdatabase::TestDatabase;
+
+    #[actix_rt::test]
+    async fn get_unknown_user() {
+        let test_database = TestDatabase::default();
+        let database = Arc::new(Database::new(&crate::database::Config {
+            url: test_database.url,
+        }));
+        let sut = UsersService::new(database);
+
+        let user_id = "2caefb5e-712c-4e99-8d18-199c344cc311".parse().unwrap();
+
+        let result = sut.get_user(user_id).await;
+        check!(result.is_none());
+    }
+}
