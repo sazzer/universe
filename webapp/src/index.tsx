55188ebe3b55d6d57554655c1a845d5b9b8a8573
Graham Cox <graham@grahamcox.co.uk>
2020-12-23 16:46:58 +0000

    Set up react-router and react-query

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -4,10 +4,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import { App } from "./ui";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
