24b82e6fdfc2e796f48b4eac8cda911afa856f19
Graham Cox <graham@grahamcox.co.uk>
2020-12-23 16:42:32 +0000

    Set up i18n

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,9 +1,14 @@
+import "./i18n";
+
 import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
+import { App } from "./ui";
 
 ReactDOM.render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
