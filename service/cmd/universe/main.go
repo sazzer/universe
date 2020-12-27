0f749c96eabe3c834a680b5eb14fc98472d87112
Graham Cox <graham@grahamcox.co.uk>
2020-12-27 20:13:44 +0000

    Set up logging

--- service/cmd/universe/main.go
+++ service/cmd/universe/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	debug := true
+
+	if debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339}).With().Caller().Logger()
+	} else {
+		log.Logger = zerolog.New(os.Stderr).With().Timestamp().Caller().Logger()
+	}
+
+	log.Info().Msg("Hello")
 }
--- service/go.mod
+++ service/go.mod
@@ -1,3 +1,5 @@
 module github.com/sazzer/universe/service
 
 go 1.15
+
+require github.com/rs/zerolog v1.20.0
