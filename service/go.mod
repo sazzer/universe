f4cfacee1c8028c9b6286d7a70392c67f358ea41
Graham Cox <graham@grahamcox.co.uk>
2020-12-28 11:01:33 +0000

    Set up Echo

--- service/go.mod
+++ service/go.mod
@@ -5,5 +5,11 @@ go 1.15
 require (
 	github.com/joho/godotenv v1.3.0
 	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/labstack/echo/v4 v4.1.17
+	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/rs/zerolog v1.20.0
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
+	golang.org/x/sys v0.0.0-20201223074533-0d417f636930 // indirect
+	golang.org/x/text v0.3.4 // indirect
 )
--- service/internal/server/server.go
+++ service/internal/server/server.go
@@ -3,21 +3,40 @@ package server
 import (
 	"fmt"
 
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/rs/zerolog/log"
 )
 
 // Server represents the HTTP server that is processing incoming requests.
-type Server struct{}
+type Server struct {
+	e *echo.Echo
+}
 
 // New will create a new instance of the server ready to run.
 func New() Server {
 	log.Info().Msg("Building HTTP Server")
 
-	return Server{}
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		ExposeHeaders: []string{"link"},
+	}))
+	e.Use(middleware.Gzip())
+	e.Use(middleware.RequestID())
+
+	return Server{e}
 }
 
 // Start the service listening on the provided HTTP port.
 func (s Server) Start(port uint16) {
 	address := fmt.Sprintf(":%d", port)
-	log.Debug().Str("address", address).Msg("Starting server")
+	log.Debug().Str("address", address).Msg("Starting HTTP server")
+
+	err := s.e.Start(address)
+	if err != nil {
+		log.Fatal().Str("address", address).Err(err).Msg("Failed to start HTTP server")
+	}
 }
