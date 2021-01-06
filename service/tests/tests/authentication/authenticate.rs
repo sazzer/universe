f355d3a822cc4d4a7976af96128407f96b4b51d1
Graham Cox <graham@grahamcox.co.uk>
2021-01-06 08:16:43 +0000

    Integration test for authentication success

--- service/tests/tests/authentication/authenticate.rs
+++ service/tests/tests/authentication/authenticate.rs
@@ -1,9 +1,66 @@
 use crate::TestService;
 use actix_web::test::TestRequest;
 use assert2::check;
+use regex::Regex;
 use std::collections::HashMap;
 use test_case::test_case;
 use universe_testdatabase::seed::SeedUser;
+
+#[actix_rt::test]
+async fn correct_password() {
+    let test_user = SeedUser {
+        user_id: "d6fc9429-1ee8-49f5-a218-b3d42b74de21".parse().unwrap(),
+        username: "known".to_owned(),
+        ..SeedUser::default()
+    }
+    .with_password("correct");
+
+    let sut = TestService::new().await;
+    sut.seed(&test_user).await;
+
+    let mut form = HashMap::new();
+    form.insert("username", "known");
+    form.insert("password", "correct");
+
+    let response = sut
+        .inject(
+            TestRequest::post()
+                .uri("/authentication/authenticate")
+                .set_json(&form)
+                .to_request(),
+        )
+        .await;
+
+    check!(response.status == 200);
+    check!(response.headers.get("content-type").unwrap() == "application/hal+json");
+    insta::assert_json_snapshot!(response.to_json().unwrap(), {
+      ".accessToken" => insta::dynamic_redaction(|value, _| {
+        let value = value.as_str().unwrap();
+        let regex = Regex::new(r#"^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$"#).unwrap();
+        check!(regex.is_match(value));
+
+        "<redactedAccessToken>"
+      }),
+      ".expires" => insta::dynamic_redaction(|value, _| {
+        // 2022-01-06T07:25:35Z
+        let value = value.as_str().unwrap();
+        let regex = Regex::new(r#"^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\\.[0-9]+)?(Z)?$"#).unwrap();
+        check!(regex.is_match(value));
+
+        "<redactedExpires>"
+      }),
+    }, @r###"
+    {
+      "accessToken": "<redactedAccessToken>",
+      "expires": "<redactedExpires>",
+      "_links": {
+        "related": {
+          "href": "/users/d6fc9429-1ee8-49f5-a218-b3d42b74de21"
+        }
+      }
+    }
+    "###);
+}
 
 #[actix_rt::test]
 async fn unknown_user() {
